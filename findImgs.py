c70605ecdc4f43cfbe9f934701a8bb31cabf81a4
Kady Zhou <kzho6737@uni.sydney.edu.au>
2024-01-18 12:53:17 +1100

    loop through URLs

--- findImgs.py
+++ findImgs.py
@@ -1,11 +1,18 @@
 # TODO - handle the current BC (minus a day???)
 
-# https://upload.neopets.com/beauty/images/winners/Poysion-2017-09-29.gif
-
 import datetime
+import requests
 
 # Starting date of BC
 start_date = datetime.date(2000, 11, 27)  # Year, Month, Day
+
+# The BC to the best of my knowledge only allows these
+FILE_EXTENSIONS = [".jpg", ".gif"]
+
+URL_BASE ="https://upload.neopets.com/beauty/images/winners/"
+
+# Set petname to loop through
+PET_NAME = "Poysion"
 
 def add_week(date):
     return date + datetime.timedelta(days=7)
@@ -33,10 +40,24 @@ def handle_bc_irregularities(date):
 
     return date
 
+bc_entries = []
+def check_url(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            bc_entries.append(url)
+            return "URL is valid and returns a resource."
+        # else:
+        #     return f"URL returned a status code of {response.status_code}."
+    except requests.RequestException as e:
+        return f"An error occurred: {e}"
+
+# This requests library is so slow!
+start_date = datetime.date(2015, 11, 27)
+
 # Set dates to loop through
 current_date = start_date
 today = datetime.date.today()
-
 
 # Loop through dates
 while current_date <= today:
@@ -44,6 +65,11 @@ while current_date <= today:
 
     # Adjust date if it's an irregular case
     current_date = handle_bc_irregularities(current_date)
-
     # Move to the next week
     current_date += datetime.timedelta(days=7)
+
+    for ext in FILE_EXTENSIONS:
+        current_date_url = URL_BASE + PET_NAME + "-" + str(current_date) + ext
+        check_url(current_date_url)
+
+print(bc_entries)
